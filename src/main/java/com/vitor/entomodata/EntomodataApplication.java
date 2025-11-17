5daca436cd18d32850b40dcf0e29561872019846
Vitor Valcarcel <vitorvalcarcel@gmail.com>
2025-11-17 16:13:53 -0300

    Primeiro commit

---
+++ src/main/java/com/vitor/entomodata/EntomodataApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.entomodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntomodataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntomodataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitor/entomodata/EntomodataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.entomodata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntomodataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
